dd0586fcc5d2cc8b1cca5d2ac4baa0b21fea00f4
Péter Sturm <sturm.peti@gmail.com>
2020-05-05 16:01:09 +0200

    Created base data structures
    Related Work Items: #47

---
+++ Lunitor.DataReader/Models/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lunitor.DataReader.Models
+{
+    class Data : IComparable<Data>
+    {
+        public DateTime TimeStamp { get; private set; }
+        public double Value { get; private set; }
+
+        public Data(DateTime timeStamp, double value)
+        {
+            TimeStamp = timeStamp;
+            Value = value;
+        }
+
+        public int CompareTo(Data other)
+        {
+            return TimeStamp.CompareTo(other.TimeStamp);
+        }
+    }
+}
---
+++ Lunitor.DataReader/Models/Parameter.cs
@@ -0,0 +1,15 @@
+﻿namespace Lunitor.DataReader.Models
+{
+    class Parameter
+    {
+        public string Name { get; set; }
+        public string UnitofMeasure { get; set; }
+        public double Min { get; set; }
+        public double Max { get; set; }
+
+        public Parameter(string name)
+        {
+            Name = name;
+        }
+    }
+}
