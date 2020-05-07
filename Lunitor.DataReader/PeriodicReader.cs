bd661f56a6c275e7ffa0bd340a2a2bea4b84d724
Péter Sturm <sturm.peti@gmail.com>
2020-05-07 13:12:34 +0200

    Implemented periodic reading and added related configurations to appsettings
    Related Work Items: #51

--- Lunitor.DataReader/PeriodicReader.cs
+++ Lunitor.DataReader/PeriodicReader.cs
@@ -1,28 +1,55 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Lunitor.DataReader
 {
-    public class PeriodicReader : BackgroundService
+    internal class PeriodicReader : BackgroundService
     {
         private readonly ILogger<PeriodicReader> _logger;
+        private readonly IHardwareMonitorLogReader _hardwareLogReader;
+        private readonly string _filePath;
+        private readonly int _periodicity;
 
-        public PeriodicReader(ILogger<PeriodicReader> logger)
+        public PeriodicReader(ILogger<PeriodicReader> logger, IHardwareMonitorLogReader hardwareLogReader, IConfiguration configuration)
         {
             _logger = logger;
+            _hardwareLogReader = hardwareLogReader;
+            _filePath = configuration.GetValue<string>("Reader:FilePath");
+            _periodicity = configuration.GetValue<int>("Reader:Periodicity");
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting with {periodicity}s periodicity", _periodicity);
+            return base.StartAsync(cancellationToken);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                _logger.LogInformation("Running at: {time}", DateTimeOffset.Now);
+                try
+                {
+                    _logger.LogInformation("Reading hardware log from {filePath}", _filePath);
+                    var hardwareLog = await File.ReadAllLinesAsync(_filePath);
+
+                    var data = _hardwareLogReader.Read(hardwareLog);
+
+                    _logger.LogInformation("Hardware log file processed: {parameterCount} paramter with {dataCount} data point/paramter", data.Keys.Count, data.Values.FirstOrDefault()?.Count);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogWarning("Failed to read/process hardware log file", ex);
+                }
+
+                await Task.Delay(_periodicity*1000, stoppingToken);
             }
         }
     }
