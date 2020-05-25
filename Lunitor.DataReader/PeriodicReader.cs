d78c8d007940558cfa13d63013e43516860ff412
Péter Sturm <sturm.peti@gmail.com>
2020-05-25 11:43:58 +0200

    Logging HarwareMonitor failure

--- Lunitor.DataReader/PeriodicReader.cs
+++ Lunitor.DataReader/PeriodicReader.cs
@@ -49,6 +49,7 @@ namespace Lunitor.DataReader
                 }
                 catch (Exception ex)
                 {
+                    _logger.LogError(ex, $"{nameof(HardwareMonitor)} failed to read get hardware data");
                     _hardwareMonitor.Stop();
                 }
 
