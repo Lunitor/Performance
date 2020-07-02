7df13b50a24981a97429263ce5ea6dcac0f9c297
Péter Sturm <sturm.peti@gmail.com>
2020-07-02 11:14:41 +0200

    implement simple grapql query
    Related Work Items: #38

--- Lunitor.Api/GraphQL/SensorReadingQuery.cs
+++ Lunitor.Api/GraphQL/SensorReadingQuery.cs
@@ -1,8 +1,19 @@
 ﻿using GraphQL.Types;
+using Lunitor.Api.Cache;
+using Lunitor.Api.GraphQL.Types;
 
 namespace Lunitor.Api.GraphQL
 {
     internal class SensorReadingQuery : ObjectGraphType
     {
+        private readonly ISensorCacheReader _sensorCache;
+
+        public SensorReadingQuery(ISensorCacheReader sensorCache)
+        {
+            _sensorCache = sensorCache;
+
+            Field<ListGraphType<SensorReadingType>>("sensorreadings",
+                resolve: context => _sensorCache.GetAll());
+        }
     }
 }
