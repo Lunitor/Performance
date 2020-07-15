6bcf53443a1e6fd6710ad5fcf5c8755fcb221678
Péter Sturm <sturm.peti@gmail.com>
2020-07-15 10:47:49 +0200

    refactored unit tests
    Related Work Items: #68

--- Lunitor.Frontend/test/components/ChartsMenu.test.tsx
+++ Lunitor.Frontend/test/components/ChartsMenu.test.tsx
@@ -3,49 +3,47 @@ import * as React from "react";
 import { ChartsMenu } from '../../components/ChartsMenu';
 import '@testing-library/jest-dom/extend-expect'
 
-describe("ChartsMenu", () => {
-    it("Should render a button for each hardware", () => {
-        const hardwares: [string, boolean][] = [
-            ["cpu", true],
-            ["ethernet 2", true],
-            ["hdd", true]
-        ];
+test("Should render a button for each hardware", () => {
+    const hardwares: [string, boolean][] = [
+        ["cpu", true],
+        ["ethernet 2", true],
+        ["hdd", true]
+    ];
 
 
-        const handleClick = function (hardwareName: string) {
-        };
+    const handleClick = function (hardwareName: string) {
+    };
 
-        render(<ChartsMenu
-            hardwares={hardwares}
-            handleClick={handleClick}
-        />);
+    render(<ChartsMenu
+        hardwares={hardwares}
+        handleClick={handleClick}
+    />);
 
-        const buttonCount = screen.getAllByRole('button').length
+    const buttonCount = screen.getAllByRole('button').length
 
-        expect(buttonCount).toEqual(hardwares.length);
-    });
+    expect(buttonCount).toEqual(hardwares.length);
+});
 
-    it("Should have buttons with all the hardwares names", () => {
-        const hardwares: [string, boolean][] = [
-            ["cpu", true],
-            ["ethernet 2", true],
-            ["hdd", true]
-        ];
+test("Should show hardware names on buttons", () => {
+    const hardwares: [string, boolean][] = [
+        ["cpu", true],
+        ["ethernet 2", true],
+        ["hdd", true]
+    ];
 
 
-        const handleClick = function (hardwareName: string) {
-        };
+    const handleClick = function (hardwareName: string) {
+    };
 
-        render(<ChartsMenu
-            hardwares={hardwares}
-            handleClick={handleClick}
-        />);
+    render(<ChartsMenu
+        hardwares={hardwares}
+        handleClick={handleClick}
+    />);
 
-        const buttonTexts = screen.getAllByRole('button')
-            .map(x => x.textContent);
+    const buttonTexts = screen.getAllByRole('button')
+        .map(x => x.textContent);
 
-        for (var i = 0; i < hardwares.length; i++) {
-            expect(buttonTexts).toContain(hardwares[i][0]);
-        }
-    });
+    for (var i = 0; i < hardwares.length; i++) {
+        expect(buttonTexts).toContain(hardwares[i][0]);
+    }
 });
